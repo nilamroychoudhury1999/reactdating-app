0fc3786e72ca635dbefd612edb959f6125387351
Nilamroychoudhury1999 <63612266+nilamroychoudhury1999@users.noreply.github.com>
2022-12-18 13:52:08 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,7 @@ import React,{useState} from "react";
 import Navbar from "./Navbar"
 import Search from "./Search"
 import Chats from "./Chats"
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import {  ZoomInSharp} from '@mui/icons-material';
+
 
 const Sidebar = () => {
   const  [value,setValue]=useState()
@@ -13,18 +11,7 @@ const Sidebar = () => {
     <>  <Navbar />
       <Search/></>
       <Chats/>
-      {/* <BottomNavigation
-  showLabels
-  value={value}
-  onChange={(event, newValue) => {
-    setValue(newValue);
-    console.log(value)
-  }}
->
-  <BottomNavigationAction label="Recents" icon={< ZoomInSharp />} />
-  <BottomNavigationAction label="Favorites" icon={< ZoomInSharp/>} />
-  <BottomNavigationAction label="Nearby" icon={< ZoomInSharp/>} />
-</BottomNavigation> */}
+
     </div>
 
   );
